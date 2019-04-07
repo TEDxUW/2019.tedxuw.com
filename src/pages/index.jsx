c8cfd6193654dfc301ca27a9f7c0f5d3dc9f8d37
Alex Xie <alexieyizhe@gmail.com>
2019-04-07 14:23:57 -0400

    Fix build

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import GlobalStyles from "~components/GlobalStyles";
+import GlobalHead from "~components/GlobalHead";
+import theme from "~utils/theme";
 import comingSoonData from "~static/data/coming-soon";
 
 import Button from "~components/Button";
@@ -13,18 +16,24 @@ const Signup = styled.div``;
 const Background = styled.div``;
 
 export default () => (
-  <PageContainer>
-    <Header>heres the title</Header>
-    <Signup>
-      {comingSoonData.title}
-      <Button
-        label="Subscribe"
-        icon="arrow-right"
-        color="white"
-        backgroundColor="primary"
-        type="submit"
-      />
-    </Signup>
-    <Background>heres the background and imgs</Background>
-  </PageContainer>
+  <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyles />
+      <GlobalHead />
+      <PageContainer>
+        <Header>heres the title</Header>
+        <Signup>
+          {comingSoonData.title}
+          <Button
+            label="Subscribe"
+            icon="arrow-right"
+            color="white"
+            backgroundColor="primary"
+            type="submit"
+          />
+        </Signup>
+        <Background>heres the background and imgs</Background>
+      </PageContainer>
+    </>
+  </ThemeProvider>
 );
